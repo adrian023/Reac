bea12ad16cd042b284a017fab19ab14a913d248c
adrian023 <azfabonan@iskolarngbayan.pup.edu.ph>
2024-08-06 18:36:32 +0800

    tried fixing navlink

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { useState, useEffect } from 'react'
-import { useParams, useLoaderData, useNavigate } from 'react-router-dom'
+import { useParams, useLoaderData, useNavigate, NavLink } from 'react-router-dom'
 import  {FaArrowLeft} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import {FaMapMarker} from 'react-icons/fa'
@@ -125,11 +125,11 @@ const JobPage = ({deleteJob}) => {
             {/* <!-- Manage --> */}
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-              <a
-                href= {`/edit-job/${job.id}`}
+              <NavLink
+                to= {`/edit-job/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</a
-              >
+                >Edit Job
+                </NavLink>
               <button
                 onClick={ ()=> onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
